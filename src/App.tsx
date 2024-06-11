51ce201feb765a3eba44575f1a55c2f215a86d05
namiron <102922048+namiron@users.noreply.github.com>
2024-06-11 20:35:40 +0300

    fix-package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import React from 'react'
-
 import './reset.scss'
 import WindowPage from './components/WindowPage'
 
--- src/components/WindowPage.tsx
+++ src/components/WindowPage.tsx
@@ -4,24 +4,25 @@ import Navigation from './windowModules/Navigation';
 import Settings from './windowModules/Settings';
 import Header from './windowModules/Header';
 import Footer from './windowModules/Footer';
+import wrapper from '../modules/wrapper.module.scss'
+
 
 const WindowPage: React.FC = () => {
 
     return (
         <div>
-            <div className="wrapper">
-                <header className="header">
+            <div className={wrapper.wrapper}>
+                <header className={wrapper.header}>
                     <Header />
                 </header>
-                <main className="main">
+                <main className={wrapper.main}>
                     <Navigation />
                     <Container />
                     <Settings />
                 </main>
-                <footer className="footer">
+                <footer className={wrapper.footer}>
                     <Footer />
                 </footer>
-
             </div>
         </div>
     )
